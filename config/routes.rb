dc85bae080f61380be70c2bdd73c4b2f581e3dc0
Yuri Delgado <yuri.m.delgado@gmail.com>
2019-10-23 18:30:01 -0500

    adding attendances model, controller, actions responses and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-    devise_for  :users,
-                  path: '',
-                  path_names: {
-                    sign_in: 'login',
-                    sign_out: 'logout',
-                    registration: 'signup'
-                },
-                controllers: {
-                  sessions: 'sessions',
-                  registrations: 'registrations'
-                }
+  
+  resources :users do
+    resources :attendances, only: [:index, :create, :update]
+  end
+
+  devise_for  :users,
+                path: '',
+                path_names: {
+                  sign_in: 'login',
+                  sign_out: 'logout',
+                  registration: 'signup'
+              },
+              controllers: {
+                sessions: 'sessions',
+                registrations: 'registrations'
+              }
 end
---
+++ spec/factories/attendances.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :attendance do
+    note { "MyString" }
+    user { nil }
+  end
+end
