61c5935b42fe6ee07832aa9fb4dc76abfbd1502e
Yuri Delgado <yuri.m.delgado@gmail.com>
2019-10-24 23:35:12 -0500

    adding create and update employees controller actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+
+  def all_employees
+    employees.all.includes(:attendances)
+  end
 end
